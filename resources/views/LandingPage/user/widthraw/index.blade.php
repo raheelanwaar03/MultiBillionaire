89a1f6cc3e63459de57e858f9cc0cc7dc848bb83
raheelanwaar03 <raheelanwaar0316@gmail.com>
2023-07-02 00:41:55 +0500

    new changes

--- resources/views/LandingPage/user/widthraw/index.blade.php
+++ resources/views/LandingPage/user/widthraw/index.blade.php
@@ -8,19 +8,42 @@
             <div class="d-flex align-items-center mt-30">
                 <div class="d-flex flex-grow">
                     <div class="mr-auto">
-                        <h1 class="b-val">${{ auth()->user()->balance }}</h1>
-                        <p class="g-text mb-0">Total Balance</p>
+                        @if (auth()->user())
+                            <h1 class="b-val"> ${{ auth()->user()->balance }} </h1>
+                            <p class="g-text mb-0">Total Balance</p>
+                        @else
+                            <h1 class="b-val"> $0.0 </h1>
+                            <p class="g-text mb-0">Total Balance</p>
+                        @endif
+
                     </div>
                     <div class="ml-auto align-self-end">
-                        <a href="#" class="profile-av"><img
-                                src="{{ asset('assets/img/content/avatar/avatar.png') }}"></a>
+                        @if (auth()->user())
+                            <h3 class="text-white">{{ auth()->user()->level }}</h3>
+                        @else
+                            <h3 class="text-white">level 0</h3>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <p>You can widthraw minimum 10$ per day with widthrawal fees of 10%. If your widthraw faild
+                                please check your giving details again and your approve widthraw will be in your wallet
+                                withing
+                                working 20 hrs.</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
 
         <section class="trans-sec container">
-            <div class="container">
+            <div class="container pb-5">
                 <div class="row">
                     <div class="col-md-8 mx-auto">
                         <div class="card">
